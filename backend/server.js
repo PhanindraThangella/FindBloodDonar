684b2cb354eb35d1b4d531789dc085cf762dd09b
RajThangella <phanindrathangella@gmail.com>
2026-04-17 17:56:59 +0530

    added fallback port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const userRoutes= require('./routes/userRoutes');
 const ConnectDB =require('./models/dbConnect');
 const app=express();
 dotenv.config();
-const port=process.env.PORT;
+const port=process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
